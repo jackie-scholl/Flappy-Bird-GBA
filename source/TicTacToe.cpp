4d0208dc978ccf1a506a05ed9b283cf8cf50c464
Jackie Scholl <jackie.h.scholl@gmail.com>
2017-04-28 23:15:01 -0400

    Cleanup

--- source/TicTacToe.cpp
+++ source/TicTacToe.cpp
@@ -1,8 +1,6 @@
-//#include <iostream>
+
 #include "TicTacToe.h"
-//#include "TicTacToeVisuals.hpp"
 #include "TicTacToeVisuals2.hpp"
-//#include "tonc_demo_lib/input.h"
 #include "tonc_demo_lib/toolbox.h"
 
 #include "TicTacToeAI.hpp"
@@ -35,10 +33,6 @@ TicTacToe::~TicTacToe()
 
 short TicTacToe::sumWithStride( short startAt, short stride ) const {
     short sum = 0;
-    // DONE TODO: (6) There is a conceptual bug below. Fix it!!!
-    //for ( unsigned i = startAt; i < boardState.size(); i+=stride ) {
-    //    sum += boardState[i];
-    //}
     sum  = boardState[startAt         ];
     sum += boardState[startAt+  stride];
     sum += boardState[startAt+2*stride];
@@ -52,7 +46,7 @@ char TicTacToe::getAt(short c, short r) const {
     if (  valAtLoc ==  0 ) {return ' ';}
     if (  valAtLoc ==  1 ) {return 'X';}
     if (  valAtLoc == -1 ) {return 'O';}
-    return                         ' '; // TODO: (7) If there is a better way, do so.
+    return                         ' ';
 }
 
 short TicTacToe::getOpenCount() const {
@@ -64,13 +58,11 @@ short TicTacToe::getOpenCount() const {
 }
 
 void TicTacToe::setAt(short c, short r, char l) {
-    // DONE TODO: (1) Fill this in such that it is consistent with the above function
     if      (l == ' ') { boardState[3*r+c] =  0; }
     else if (l == 'X') { boardState[3*r+c] =  1; }
     else if (l == 'O') { boardState[3*r+c] = -1; }
 }
 
-// DONE TODO (3) add an implementation of getWinner( short sum ) here
 char TicTacToe::getWinner( short sum ) const { // returns 'X','O' if sum=3,-3 respectively, else returns ' '
     if      ( sum >=  3 ) return 'X';
     else if ( sum <= -3 ) return 'O';
@@ -85,14 +77,12 @@ char TicTacToe::getWinner() const {
     check = getWinner( sumWithStride(2,3) ); if ( check != ' ' ) { return check; }
 
     // check rows
-    // DONE TODO: (4)
     check = getWinner( sumWithStride(0,1) ); if ( check != ' ' ) { return check; }
     check = getWinner( sumWithStride(3,1) ); if ( check != ' ' ) { return check; }
     check = getWinner( sumWithStride(6,1) ); if ( check != ' ' ) { return check; }
 
     // check diagonals
     check = getWinner( sumWithStride(2,2) ); if ( check != ' ' ) { return check; }
-    // DONE TODO: (5) check the other diagonal
     check = getWinner( sumWithStride(0,4) ); if ( check != ' ' ) { return check; }
 
     // return if nothing found
@@ -102,13 +92,6 @@ char TicTacToe::getWinner() const {
 bool TicTacToe::operator==(const TicTacToe &other) const {
   return (boardState == other.boardState);
 }
-
-/*#define REG_VCOUNT *(vu16*)0x04000006
-
-void vid_vsync() {
-    while(REG_VCOUNT >= 160) {}   // wait till VDraw
-    while(REG_VCOUNT < 160) {}    // wait till VBlank
-}*/
 
 void TicTacToe::printState() const {
     md5_update();
@@ -139,26 +122,14 @@ void TicTacToe::playGameOnCommandLine() {
     while( (openCount=getOpenCount())>0 and getWinner() == ' ' ) {
         printState();
 
-        if (openCount%2==1) { /*std::cout << "It is X's move " << std::endl;*/ }
-        else                { /*std::cout << "It is O's move " << std::endl;*/ }
-
         if (openCount%2==1) {
-          //std::cout << "Please enter column and row numbers separated by space, e.g., 1 2 or 3 1 " << std::endl;
-          //std::cin >> c >> r;
-          //c = 0;
-          //r = 0;
-          //highlight(0, 1, CLR_LIME);
           short val = getUserInputSquare(*this);
-          //short val = getSquare(*this); // 'X'
           c = val / 3;
           r = val % 3;
-          //highlight(0, 2, CLR_LIME);
         } else  {
-          //highlight(3, 2, CLR_MAG);
           short val = getSquare(*this); // 'O'
           c = val / 3;
           r = val % 3;
-          //highlight(3, 2, CLR_WHITE);
         }
 
         if ( getAt(c,r) == ' ' ) {
@@ -166,12 +137,6 @@ void TicTacToe::playGameOnCommandLine() {
             if (openCount%2==1) { setAt(c,r,'X'); }
             else                { setAt(c,r,'O'); }
         }
-        //highlight(3, 2, CLR_YELLOW);
-        /*for (int i=0; i<60; i++) {
-          //vid_vsync();
-          printState();
-        }*/
-        //highlight(2, 2, CLR_WHITE);
     }
     printState();
     if     ( getWinner() == 'X' ) { game_end2(+1); }
--- source/main.cpp
+++ source/main.cpp
@@ -1,34 +1,9 @@
 #include "TicTacToe.h"
-//#include "TicTacToeVisuals.hpp"
-//#include <iostream>
-
-//#include <unordered_map>
 
 #include "TextDemo.hpp"
 #include "Timer.hpp"
-//#include "md5.h"
-
-//#include <string.h>
-
-//int main_1();
-
-//std::vector<TicTacToe> getPossibleNexts(TicTacToe board, char player);
-//short getValue(TicTacToe board, char player);
 
 int main(int argc, char **argv) {
-    //init_time();
-  //main_1();
-    //setup();
-    //printText("\x1b[0;0HHello\nWorld\n!\n");
-
-    //printText("\x1b[0;0HHello\nWorld\n!\n");
-
-
-    //reset_time();
-    //md5_loop();
-    //print_time();
-
-
     while(1) {
       TicTacToe myGame;
       myGame.playGameOnCommandLine();
